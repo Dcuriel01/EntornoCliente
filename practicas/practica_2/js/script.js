2ce5cd2fb0ec07515cf6a8caf4ee1d59f01a9ff0
Dcuriel01 <dcurielm01@educarex.es>
2024-09-17 09:44:08 +0200

    Tarea 2

--- practicas/practica_2/js/script.js
+++ practicas/practica_2/js/script.js
@@ -1,19 +1,12 @@
 window.onload = () => {
-
-    
     const b1 = document.getElementById('btn1')
     const b2 = document.getElementById('btn2')
     const b3 = document.getElementById('btn3')
     const b4 = document.getElementById('btn4')
 
     const ul = b1.previousElementSibling
-    const li1 = ul.firstElementChild
-    const li2 = li1.nextElementSibling
-    const li3 = li2.nextElementSibling
-    const li4 = li3.nextElementSibling
-    const li5 = li4.nextElementSibling
 
-    let lis = [li1,li2,li3,li4,li5]
+    let lis = ul.children
     b1.addEventListener("click",accion1)
     b2.addEventListener("click",accion2)
     b3.addEventListener("click",accion3)
@@ -22,16 +15,15 @@ window.onload = () => {
     function accion1(){
         resetColores()
         for (let index = 0; index < lis.length; index++) {
-        const random = Math.floor(Math.random()*1000)
+        const random = generarNum(1000)
         lis[index].textContent=random
-        
         }
     }
-
+    
     function accion2(){
         
         for (let index = 0; index < lis.length; index++) {
-        const random = Math.floor(Math.random()*1000)
+        const random = generarNum(1000)
         lis[index].textContent=random
         
         if (lis[index].textContent < 500) {
@@ -41,19 +33,24 @@ window.onload = () => {
         }
         }
     }
-
+    
     function accion3(){
         resetColores()
         for (let index = 0; index < lis.length; index++) {
-        const random = Math.floor(Math.random()*1000)
+        const random = generarNum(1000)
+        const color = generarColor()
         lis[index].textContent=random
-        
+        lis[index].style.color=color
         }
     }
-
+    
     function accion4(){
-        const n1 = Math.floor(Math.random()*10)
-        const n2 = Math.floor(Math.random()*10)
+        resetNum()
+        resetColores()
+        const n1 = generarNum(10)
+        const n2 = generarNum(10)
+        lis[0].textContent=n1
+        lis[1].textContent=n2
         if (n1===n2) {
             alert("Los numeros son iguales y son: " + n1 )
         }
@@ -64,4 +61,25 @@ window.onload = () => {
             lis[index].style.color=null
         }
     }
+
+    function resetNum(){
+        for (let index = 0; index < lis.length; index++) {
+            lis[index].textContent=null
+        }
+    }
+
+    
 }
+function generarNum(max){
+    const random = Math.floor(Math.random()*max+1)
+    return random
+}
+function generarColor(){
+    const r = generarNum(255)
+    const g = generarNum(255)
+    const b = generarNum(255)
+    const color = "RGB(" + r + "," + g + "," + b + ")"
+    return color
+}
+
+
